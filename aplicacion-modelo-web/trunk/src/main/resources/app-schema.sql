9626359598948c01176270abd46d139145aad0f1
ldeseta <ldeseta@beaecbbf-dd37-633f-ad26-fb83c37b78b3>
2011-03-29 17:19:20 +0000

    arreglos a los scripts de base de datos.

--- aplicacion-modelo-web/trunk/src/main/resources/app-schema.sql
+++ aplicacion-modelo-web/trunk/src/main/resources/app-schema.sql
@@ -1,1 +1,8 @@
-create table personas (id bigint PRIMARY KEY, nombre varchar(100), apellido varchar(100), email varchar(50));
+drop table personas if exists;
+
+create table personas (
+    id bigint identity PRIMARY KEY, 
+    nombre varchar(100), 
+    apellido varchar(100), 
+    email varchar(50) not null
+);
