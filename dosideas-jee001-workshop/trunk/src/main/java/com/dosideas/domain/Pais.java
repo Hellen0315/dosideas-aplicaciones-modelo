20d2e793022da041fc8ad4ca4b984a56662b5f49
ldeseta@gmail.com <ldeseta@gmail.com@beaecbbf-dd37-633f-ad26-fb83c37b78b3>
2011-06-17 17:31:40 +0000

    Hibernate ahora se configura con anotaciones.

--- dosideas-jee001-workshop/trunk/src/main/java/com/dosideas/domain/Pais.java
+++ dosideas-jee001-workshop/trunk/src/main/java/com/dosideas/domain/Pais.java
@@ -1,40 +1,51 @@
 package com.dosideas.domain;
 
 import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  *
  * @author ldeseta
  */
+@Entity
+@Table(name = "PAIS")
 public class Pais implements Serializable {
-
+    
+    @Id
+    @Column(name = "ID_PAIS")
     private Long id;
+    
+    @Column(name = "NOMBRE")
     private String nombre;
-	/**
-	 * @return the id
-	 */
-	public Long getId() {
-		return id;
-	}
-	/**
-	 * @param id the id to set
-	 */
-	public void setId(Long id) {
-		this.id = id;
-	}
-	/**
-	 * @return the descripcion
-	 */
-	public String getNombre() {
-		return nombre;
-	}
-	/**
-	 * @param descripcion the descripcion to set
-	 */
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
 
+    /**
+     * @return the id
+     */
+    public Long getId() {
+        return id;
+    }
 
+    /**
+     * @param id the id to set
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    /**
+     * @return the descripcion
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param descripcion the descripcion to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
