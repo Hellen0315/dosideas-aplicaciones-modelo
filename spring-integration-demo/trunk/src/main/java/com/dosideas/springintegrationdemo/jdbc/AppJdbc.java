a519bf1491ab5f8575d27035187abd825e2199a0
cocoriv@gmail.com <cocoriv@gmail.com@beaecbbf-dd37-633f-ad26-fb83c37b78b3>
2011-07-05 15:33:47 +0000

    Arreglos y docuemntación.

--- spring-integration-demo/trunk/src/main/java/com/dosideas/springintegrationdemo/jdbc/AppJdbc.java
+++ spring-integration-demo/trunk/src/main/java/com/dosideas/springintegrationdemo/jdbc/AppJdbc.java
@@ -4,8 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
- *
+ * Demo de manejo de sql con Spring Integration.
+ * En el archivo jdbc-context.xml hay configurado un jdbc:inbound-channel-adapter
+ * que mediante una consulta sql obtiene un mensaje con la colección de registros 
+ * de una tabla. La colección del mensaje se divide en mensajes individuales y 
+ * dependiendo del valor de una columna se serializa de determinada manera.
+ * Se utiliza una base de datos en memoria HQL.
+ * 1) Correr la clase.
+ * 2) Controlar que por consola se muestren los registros serializados según 
+ *    el valor de la columna formato.
  */
 public class AppJdbc 
 {
