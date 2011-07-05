1752457204dc85618bc3fd5ca77bd41bd6a81ac8
cocoriv@gmail.com <cocoriv@gmail.com@beaecbbf-dd37-633f-ad26-fb83c37b78b3>
2011-07-05 18:40:39 +0000

    Se agrega ejemplo de socket tcp.

--- spring-integration-demo/trunk/target/classes/jdbc/app-datos.sql
+++
@@ -1,6 +0,0 @@
-insert into mensajes (id, nombre, apellido, email) values(1, 'Mensaje_1', 0);
-insert into mensajes (id, nombre, apellido, email) values(2, 'Mensaje_2', 0);
-insert into mensajes (id, nombre, apellido, email) values(3, 'Mensaje_3', 0);
-insert into mensajes (id, nombre, apellido, email) values(4, 'Mensaje_4', 0);
-insert into mensajes (id, nombre, apellido, email) values(5, 'Mensaje_5', 0);
-
