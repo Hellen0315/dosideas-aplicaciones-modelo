a4129d1ee6e42d296f7dd488ceabd27425be4ed7
cocoriv <cocoriv@gmail.com>
2011-07-05 15:33:47 +0000

    Arreglos y docuemntación.

--- spring-integration-demo/trunk/src/main/java/com/dosideas/springintegrationdemo/file/AppFile.java
+++ spring-integration-demo/trunk/src/main/java/com/dosideas/springintegrationdemo/file/AppFile.java
@@ -4,7 +4,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
+ * Demo de manejo de archivos con Spring Integration.
+ * En el archivo file-context.xml hay configurado un file:inbound-channel-adapter
+ * sobre el directorio tmp y un file:outbound-channel-adapter sobre el directorio archivos.
+ * La idea es mover los archivos que llegan al directorio tmp al directorio archivos.
+ * 1) Correr la clase.
+ * 2) Mover los archivo del directorio archivos al directorio tmp.
+ * 3) En un segundo deben aparecer nuevamente en el directorio archivos.
  *
  */
 public class AppFile 
--- spring-integration-demo/trunk/src/main/java/com/dosideas/springintegrationdemo/ftp/AppFtp.java
+++ spring-integration-demo/trunk/src/main/java/com/dosideas/springintegrationdemo/ftp/AppFtp.java
@@ -4,8 +4,21 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
- *
+ * Demo de FTP de archivos con Spring Integration.
+ * En el archivo ftp-context.xml hay configurado un ftp:inbound-channel-adapter,
+ * sobre el directorio remoto de salida, que realiza el ftp al directorio tmp 
+ * local, un publish-subscribe-channel, asociado a un ftp:inbound-channel-adapter 
+ * sobre el directorio tmp, con dos suscriptores, un ftp:outbound-channel-adapter 
+ * que realiza el ftp al directorio remoto de entrada y un file:outbound-channel-adapter 
+ * que mueve el archivo al directorio local archivos.
+ * La idea es crear un archivo en el directorio remoto de salida, este se 
+ * transmitirá al directorio local tmp y luego nuevamente se transmitirá al 
+ * directorio remoto de entrada. Por ultimo se moverá al directorio local archivos.
+ * 1) Correr la clase.
+ * 2) Crear el archivo en el directorio de salida remoto.
+ * 3) Este archivo se trasfiere al directorio local tmp y luego al directorio 
+ *    remosto de entrada.
+ * 4) Por ultimo se mueve al directorio de archivos.
  */
 public class AppFtp 
 {
