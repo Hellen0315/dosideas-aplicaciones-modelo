d7efdf34c3fe4c0078e63d932db796e3a1569140
ldeseta@gmail.com <ldeseta@gmail.com@beaecbbf-dd37-633f-ad26-fb83c37b78b3>
2011-09-02 15:24:10 +0000

    cambio del paquete "business" por "service".

---
+++ dosideas-jee001-workshop/trunk/src/main/java/com/dosideas/service/PaisService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dosideas.service;
+
+import com.dosideas.domain.Pais;
+
+/**
+ *
+ * @author ldeseta
+ */
+public interface PaisService {
+
+    Pais buscarPaisPorId(Long id);
+    
+}
