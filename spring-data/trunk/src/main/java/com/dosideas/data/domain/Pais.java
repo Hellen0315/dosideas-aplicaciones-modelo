eddd82ce004d94fafe48df382b36b1c7449eb038
ldeseta <ldeseta@gmail.com>
2012-04-26 15:08:41 +0000

    Proyecto de prueba con Spring Data y Spring Data Rest.

---
+++ spring-data/trunk/src/main/java/com/dosideas/data/domain/Pais.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dosideas.data.domain;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author ldeseta
+ */
+@Entity
+public class Pais implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
