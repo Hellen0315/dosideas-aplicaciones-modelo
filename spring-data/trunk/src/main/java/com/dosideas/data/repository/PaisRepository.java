8246a4e891bc9ed65efdb4bc70a20f8ed958f72a
cocoriv@gmail.com <cocoriv@gmail.com@beaecbbf-dd37-633f-ad26-fb83c37b78b3>
2012-05-03 16:09:11 +0000

    Fine tuning repository definition.

---
+++ spring-data/trunk/src/main/java/com/dosideas/data/repository/PaisRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dosideas.data.repository;
+
+import com.dosideas.data.domain.Pais;
+
+/**
+ * Esta interfaz no tiene implementación en el proyecto. En tiempo de ejecución 
+ * se implementaran los metodos expuestos en esta interfaz y los expuestos en la
+ * interfaz "CustomRepository".
+ * 
+ * Leer más en
+ * http://static.springsource.org/spring-data/data-jpa/docs/1.0.3.RELEASE/reference/html/#repositories.definition-tuning
+ *
+ */
+public interface PaisRepository extends CustomRepository<Pais, Long> {
+    
+}
---
+++ spring-data/trunk/src/test/java/com/dosideas/data/repository/PaisRepositoryTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dosideas.data.repository;
+
+import com.dosideas.data.domain.Pais;
+import java.util.List;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author parivero
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+    "classpath:applicationContext.xml",
+    "classpath:datasource.xml"
+})
+@Transactional
+public class PaisRepositoryTest {
+    
+    @Autowired
+    private PaisRepository paisRepository;
+    
+    @Test
+    public void findOne_idExistente_retornaPersona() {
+        long id = 1L;
+
+        Pais result = paisRepository.findOne(id);
+
+        assertNotNull(result);
+        assertEquals(id, result.getId().longValue());
+    }
+    
+    @Test
+    public void findAll_retornaListaDePaises() {
+        List<Pais> result = paisRepository.findAll();
+        assertNotNull(result);
+        assertTrue(result.size() > 0);
+    }
+    
+    
+}
