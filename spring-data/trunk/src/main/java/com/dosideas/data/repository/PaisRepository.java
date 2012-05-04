b08c949a1ffd347c4d4ee5a222b1dd278c1f1016
cocoriv@gmail.com <cocoriv@gmail.com@beaecbbf-dd37-633f-ad26-fb83c37b78b3>
2012-05-04 14:51:39 +0000

    NamedQuery en las entidades.

--- spring-data/trunk/src/main/java/com/dosideas/data/repository/PaisRepository.java
+++ spring-data/trunk/src/main/java/com/dosideas/data/repository/PaisRepository.java
@@ -5,12 +5,12 @@
 package com.dosideas.data.repository;
 
 import com.dosideas.data.domain.Pais;
-import java.util.List;
 
 /**
  * Esta interfaz no tiene implementación en el proyecto. En tiempo de ejecución
  * se implementaran los métodos expuestos en esta interfaz y los expuestos en la
- * interfaz "ReadOnlyRepository".
+ * interfaz "ReadOnlyRepository", los autogenerados por Spring Data y los
+ * namedQuery definidos en la entidad.
  *
  * Leer más en
  * http://static.springsource.org/spring-data/data-jpa/docs/1.0.3.RELEASE/reference/html/#repositories.definition-tuning
