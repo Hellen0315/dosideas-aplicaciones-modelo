d4edb435c145c2649745eef7d508d0ffc7847754
ldeseta <ldeseta@gmail.com>
2012-11-14 15:19:17 +0000

    Cambios en la nomenclatura de los métodos de test.

--- dosideas-jee001-workshop/trunk/src/test/java/com/dosideas/service/PaisServiceComponenteTest.java
+++ dosideas-jee001-workshop/trunk/src/test/java/com/dosideas/service/PaisServiceComponenteTest.java
@@ -13,19 +13,19 @@ import static org.junit.Assert.*;
 /**
  * Esta clase representa un test de componente. Los tests de componentes
  * se encargan de testear una clase en particular, y todas las dependencias
- * que la misma tiene. 
- * 
+ * que la misma tiene.
+ *
  * Este test comprueba el funcionamiento de la clase PaisDao. Esta clase
  * utiliza Hibernate para acceder a la base de datos. Es necesario tener
  * entonces la base de datos iniciada, con el modelo de datos del workshop
  * cargado (ver archivo "Instrucciones para crear la BD").
  *
- * Para iniciar la base de datos: 
+ * Para iniciar la base de datos:
  *   Ir al menu Window > Services > Databases > Java DB > Click derecho > Start server
- * 
- * Para ejecutar este test en NetBeans: 
+ *
+ * Para ejecutar este test en NetBeans:
  *    click derecho > "Test File" (CTRL + F6)
- * 
+ *
  * @author ldeseta
  */
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -38,18 +38,18 @@ import static org.junit.Assert.*;
 @Transactional
 public class PaisServiceComponenteTest {
 
-    /** La instancia bajo test. 
-     *  La anotación @Autowired hará que Spring la inyecte automáticamente 
+    /** La instancia bajo test.
+     *  La anotación @Autowired hará que Spring la inyecte automáticamente
      */
     @Autowired
     private PaisService instance;
 
     /**
-     * Test de buscarPaisPorId method con un id existente. 
+     * Test de buscarPaisPorId method con un id existente.
      * El metodo debe encontrar un Pais con el id buscado.
      */
     @Test
-    public void buscarPaisPorIdConIdExistente() {
+    public void buscarPaisPorId_ConIdExistente_retornaProvinciaConEseId() {
         Long id = 1L;
 
         Pais pais = instance.buscarPaisPorId(id);
@@ -60,11 +60,11 @@ public class PaisServiceComponenteTest {
     }
 
     /**
-     * Test de buscarPaisPorId method con un id inexistente. 
+     * Test de buscarPaisPorId method con un id inexistente.
      * El metodo debe devolver null al buscar un id que no existe.
      */
     @Test
-    public void buscarPaisPorIdConIdInexistente() {
+    public void buscarPaisPorId_ConIdInexistente_retornaNull() {
         Long id = 21L;
 
         Pais pais = instance.buscarPaisPorId(id);
@@ -73,12 +73,12 @@ public class PaisServiceComponenteTest {
     }
 
     /**
-     * Test de buscarPaisPorId method con un id null. 
+     * Test de buscarPaisPorId method con un id null.
      * El metodo debe tirar una IllegalArgumentException al intengar invocar
      * al metodo con un null.
      */
     @Test(expected = IllegalArgumentException.class)
-    public void buscarPaisPorIdConIdNull() {
+    public void buscarPaisPorId_ConIdNull_lanzaExcepcion() {
         instance.buscarPaisPorId(null);
         fail("Debería haberse lanzado una excepción.");
     }
